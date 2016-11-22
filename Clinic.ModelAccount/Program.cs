96309b4a1ad69d1a6ea7b9fe56c1a93affe644e0
lavrowlad <lavrowlad@gmail.com>
2016-11-22 20:23:51 +0200

    Create testApp
    testing accounts in Program class in Clinic.ModelAcclunt project

--- Clinic.ModelAccount/Program.cs
+++ Clinic.ModelAccount/Program.cs
@@ -8,8 +8,18 @@ namespace Clinic.ModelAccount
 {
 	class Program
 	{
+		static List< Account > accounts = new List< Account >();
+
 		static void Main ( string [ ] args )
 		{
+			accounts.Add( new Employee( AccountStatusEmployee.Administrator,    Guid.NewGuid(), "Vladyslav", "lavrowlad@gmail.com",		    "lavrowlad" ) );
+			accounts.Add( new Employee( AccountStatusEmployee.Doctor,		    Guid.NewGuid(), "Alexandra", "alexandra.bublic@clinic.com", "alexandra" ) );
+			accounts.Add( new Patient(	AccountStatusPatient.AuthorlessPatient, Guid.NewGuid(), "Maxin",	 "maxik037@rambler.ru",			"maxZal06"  ) );
+
+			Console.WriteLine("=== Accounts ===");
+			foreach ( Account account in accounts )
+				Console.WriteLine( "\n" + account.ToString() );
+			Console.WriteLine();
 		}
 	}
 }
