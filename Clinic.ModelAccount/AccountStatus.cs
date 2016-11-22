8121f20d67b11b977fc773733eaa625bbd9e9aef
lavrowlad <lavrowlad@gmail.com>
2016-11-22 20:22:17 +0200

    Create enum
    AccountStatusEmployee
    AccluntStatusPatient

---
+++ Clinic.ModelAccount/AccountStatus.cs
@@ -0,0 +1,19 @@
+﻿/* ©2016, lavrowlad, NURE */
+
+namespace Clinic.ModelAccount
+{
+	public enum AccountStatusEmployee
+	{
+		Administrator,
+		Doctor,
+		Intern,
+		Nurce,
+		Operator
+	}
+
+	public enum AccountStatusPatient
+	{
+		AuthorlessPatient,
+		RegisteredPatient
+	}
+}
